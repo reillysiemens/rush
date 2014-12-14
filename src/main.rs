a8d7eb4d6cb89c5f4c5409d52772c1230fde2293
Bethany Long <longb4@students.wwu.edu>
2014-12-14 09:57:33 -0800

    basic command execution

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 fn main() {
     loop {
         prompt::display_prompt();
-        you_typed(prompt::get_input());
+        process_line(prompt::get_input());
+        // TODO: Figure out how to exit! (Either by typing `exit [someval?]` or ^D?)
+        // In prompt::get_input, the value in .expect() is what rush "panick[s] at"
+        // when ^D is sent.
     }
 }
 
-fn you_typed(s: String) {
-    println!("You typed \"{}\"", s);
+fn process_line(s: String) {
+    let split_vec: Vec<&str> = s.split_str(" ").filter(|p| !p.is_empty()).collect();
+    execute::run(split_vec);
 }
 
 mod prompt {
@@ -24,5 +28,23 @@ mod prompt {
                         .expect("Failed to read line");
 
         input.trim().to_string()
+    }
+}
+
+mod execute {
+    use std::io::Command;
+
+    pub fn run(command_line: Vec<&str>) {
+        let mut cl = command_line.clone();
+        let command = cl[0].to_string();
+        cl.remove(0);
+        let output = match Command::new(command).args(cl.as_slice()).output() {
+            Ok(output) => output,
+            Err(e) => panic!("Failed to execute process: {}", e),
+        };
+
+        println!("Process exited with {}", output.status);
+        println!("stdout: {}", String::from_utf8_lossy(output.output.as_slice()));
+        println!("stderr: {}", String::from_utf8_lossy(output.error.as_slice()));
     }
 }
